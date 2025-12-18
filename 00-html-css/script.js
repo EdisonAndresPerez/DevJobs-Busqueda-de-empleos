16bcf5a9f359af3ad48287c4627ff0bdac4dd3d7
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-18 15:57:10 -0500

    seleccionamos los botones y los imprimos en consola gracias a class

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,6 +1,6 @@
 // Selecciona el boton por su ID
 // En el documento HTML, asegurate de que el boton tenga el atributo id="aplicar"
-const botonAplicar = document.querySelector("#aplicar");
+const botonAplicar = document.querySelectorAll(".button-apply-job");
 console.log(botonAplicar)
 
 // Agrega un evento de click al boton
