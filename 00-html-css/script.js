879d22af9c8ffdb4890c138896f70826b888a769
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-18 16:04:53 -0500

    se aplica el foreach para recorrer cada elemnto de la lista ( en este caso los botones ) y poder hacer funcional la funcion botonesAplicar

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,13 +1,15 @@
 // Selecciona el boton por su ID
 // En el documento HTML, asegurate de que el boton tenga el atributo id="aplicar"
-const botonAplicar = document.querySelectorAll(".button-apply-job");
-console.log(botonAplicar)
+const botonesAplicar = document.querySelectorAll(".button-apply-job");
+console.log(botonesAplicar);
 
 // Agrega un evento de click al boton
 // Cuando se haga click, se mostrara una alerta
-botonAplicar.addEventListener("click", function () {
-  botonAplicar.textContent = "Aplicado";
-  botonAplicar.disable = true;
-  botonAplicar.style.backgroundColor = "green";
-  botonAplicar.style.cursor = "not-allowed";
+botonesAplicar.forEach((botonAplicar) => {
+  botonAplicar.addEventListener("click", function () {
+    botonAplicar.textContent = "Aplicado";
+    botonAplicar.disable = true;
+    botonAplicar.style.backgroundColor = "green";
+    botonAplicar.style.cursor = "not-allowed";
+  });
 });
