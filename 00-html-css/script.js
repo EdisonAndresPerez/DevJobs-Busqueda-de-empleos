9b400985101725ac8f6d511c6cabdf4bdadd0333
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-18 16:59:35 -0500

    colocamos un id a la tarjeta de empleos. en el javascript usando closest para detectar el padre aunque se haga click en otro lado y imprimir en consola esa card

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,15 +1,41 @@
 // Selecciona el boton por su ID
 // En el documento HTML, asegurate de que el boton tenga el atributo id="aplicar"
-const botonesAplicar = document.querySelectorAll(".button-apply-job");
-console.log(botonesAplicar);
+//const botonesAplicar = document.querySelectorAll(".button-apply-job");
+//console.log(botonesAplicar);
 
 // Agrega un evento de click al boton
 // Cuando se haga click, se mostrara una alerta
-botonesAplicar.forEach((botonAplicar) => {
-  botonAplicar.addEventListener("click", function () {
-    botonAplicar.textContent = "Aplicado";
-    botonAplicar.disable = true;
-    botonAplicar.style.backgroundColor = "green";
-    botonAplicar.style.cursor = "not-allowed";
-  });
+//botonesAplicar.forEach((botonAplicar) => {
+//  botonAplicar.addEventListener("click", function () {
+//    botonAplicar.textContent = "Aplicado";
+//    botonAplicar.disable = true;
+//    botonAplicar.style.backgroundColor = "green";
+//    botonAplicar.style.cursor = "not-allowed";
+//  });
+//});
+
+//Forma correcta y actualizada
+
+const jobsListings = document.querySelector(".jobs-listings");
+
+jobsListings.addEventListener("click", function (evento) {
+  if (evento.target.classList.contains("button-apply-job")) {
+    evento.target.textContent = "Aplicado";
+    evento.target.disabled = true;
+    evento.target.style.backgroundColor = "green";
+    evento.target.style.cursor = "not-allowed";
+  }
+
+  console.log(evento.target.classList.contains("button-apply-job"));
+});
+
+
+
+document.addEventListener("click", (evento) => {
+  const card = evento.target.closest?.(".job-listing-card");
+  if (!card) return;
+
+  if (card.id === "job-mobile") {
+    console.log("Haz hecho click en la tarjeta: Desarrollador de Aplicaciones Móviles");
+  }
 });
