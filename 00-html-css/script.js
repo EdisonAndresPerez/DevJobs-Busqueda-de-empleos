8d340b252ed055956a37e04f7b75058c18698ac2
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-18 15:55:49 -0500

    para poder iterar una lista necesitamos usar class y no id, ya que id es solo para un elemento y no para varios. para eso vamos a class y tambien queryselectorall

--- 00-html-css/script.js
+++ 00-html-css/script.js
@@ -1,9 +1,13 @@
 // Selecciona el boton por su ID
 // En el documento HTML, asegurate de que el boton tenga el atributo id="aplicar"
 const botonAplicar = document.querySelector("#aplicar");
+console.log(botonAplicar)
 
 // Agrega un evento de click al boton
 // Cuando se haga click, se mostrara una alerta
 botonAplicar.addEventListener("click", function () {
-    botonAplicar.textContent = "Aplicado";
+  botonAplicar.textContent = "Aplicado";
+  botonAplicar.disable = true;
+  botonAplicar.style.backgroundColor = "green";
+  botonAplicar.style.cursor = "not-allowed";
 });
