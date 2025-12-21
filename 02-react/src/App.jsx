3052d0ec87e9bed4ce6dfd0c3d15e84fa337d893
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-20 20:10:27 -0500

    se crea la pagina error o 404 para las pequenias rutas que se hizo

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -3,16 +3,23 @@ import { Footer } from "./components/Footer/Footer";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Error from "./components/Error";
 
 function App() {
   const currentPage = window.location.pathname;
 
+  let page = <Error/>;
+
+  if (currentPage == "/") {
+    page = <Home />;
+  } else if (currentPage == "/search") {
+    page = <Search />;
+  }
+
   return (
     <>
       <Header />
-      {currentPage === "/" ? <Home /> : null} 
-      {currentPage === "/search" ? <Search /> : null}
-
+      {page}
       <Footer />
     </>
   );
