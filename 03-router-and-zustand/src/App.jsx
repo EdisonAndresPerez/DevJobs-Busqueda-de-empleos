03191ecefd075f5c40beb4067079e994bb85cdd7
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-28 06:28:45 -0500

    se agrega componente 404

--- 03-router-and-zustand/src/App.jsx
+++ 03-router-and-zustand/src/App.jsx
@@ -2,22 +2,22 @@ import { Header } from "./components/header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { BrowserRouter, Routes, Route } from "react-router";
 
-
+import Error from "./components/Error";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Login from "./pages/Login";
-
-
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </>
