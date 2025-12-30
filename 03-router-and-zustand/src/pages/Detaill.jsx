80f83181ef96429faef1434dd992b9258d76ba51
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-30 13:27:23 -0500

    mostrando cada trabajo por su id unico

--- 03-router-and-zustand/src/pages/Detaill.jsx
+++ 03-router-and-zustand/src/pages/Detaill.jsx
@@ -1,7 +1,23 @@
+
+import { useState, useEffect } from "react";
+
+
+//para saber la id de la ruta
 import { useParams } from "react-router-dom";
 
 const Detaill = () => {
   const { id } = useParams();
+
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    fetch(`https://jscamp-api.vercel.app/api/jobs/d35b2c89-5d60-4f26-b19a-6cfb2f1a0f57`)
+  })
+
+
 
   return (
     <>
