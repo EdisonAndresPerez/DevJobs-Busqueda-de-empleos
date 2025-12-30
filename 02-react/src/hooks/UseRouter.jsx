61a708680f553e1ab91e4c175b75f38d09949790
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2025-12-30 07:49:27 -0500

    link y hoojs useNavigate useLocation de react router

--- 02-react/src/hooks/UseRouter.jsx
+++ 02-react/src/hooks/UseRouter.jsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from "react";
+import { useNavigation } from "react-router";
 
 export function useRouter() {
-  const [currentPage, setCurrentPage] = useState(window.location.pathname);
-  const [currentSearch, setCurrentSearch] = useState(window.location.search);
+  const navigate = useNavigation();
+  const location = useNavigation();
 
-  useEffect(() => {
-    const handlePopState = () => {
-      setCurrentPage(window.location.pathname);
-      setCurrentSearch(window.location.search);
-    };
-
-    window.addEventListener("popstate", handlePopState);
-
-    return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
-  }, []);
-
-  const navigate = (to) => {
-    window.history.pushState({}, "", to);
-    window.dispatchEvent(new PopStateEvent("popstate"));
+  const navigateTo = (to) => {
+    navigate(to);
   };
 
-  return { currentPage, currentSearch, navigate };
+  return {
+    navigateTo,
+    currentPage: location.pathname,
+    currentSearch: location.search,
+  };
 }
