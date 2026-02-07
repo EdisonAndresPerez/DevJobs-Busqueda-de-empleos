f58c5c9faecade5a66558c6e8770739493f1f8d6
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-07 09:12:02 -0500

    primer paso real trabajando con Node.js: crear archivos, ejecutarlos desde la terminal y entender cómo funciona su sistema de módulos, que es uno de los puntos donde más confusión suele haber al venir del frontend.

---
+++ 04-node/app.test.js
@@ -0,0 +1,10 @@
+
+//importamos los metodos de test, describe, before y after para estructurar nuestras pruebas
+import { test, describe, before, after} from 'node:test';
+
+//importamos el modulo assert para hacer las aserciones
+//comparaciones entre valores esperados y obtenidos
+import assert from 'node:assert';
+
+
+
---
+++ 04-node/index.js
@@ -0,0 +1,6 @@
+import { sumar } from './sumar.js';
+
+console.log("Hola mundo desde Node.js");
+
+const resultado = sumar(2, 6);
+console.log("El resultado de sumar 2 + 6 es:", resultado);
