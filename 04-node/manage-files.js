62fc222b9bc6fd3160829f7d21a738c47f891dbf
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-07 10:11:38 -0500

    aprendemos a usar writefile y readfile para poder leer, escribir y crear ficheros

--- 04-node/manage-files.js
+++ 04-node/manage-files.js
@@ -1,9 +1,19 @@
 
 //leer ficheros con node
 import {readFile} from 'node:fs/promises'
-
-
-//leer un fichero
-//
 const contenido = await readFile('./archivoLeer.txt', 'utf-8')
 console.log(contenido)
+
+
+//escribir un fichero
+import {writeFile} from 'node:fs/promises'
+const nuevoContenido = 'Este es el nuevo contenido del archivo'
+await writeFile('./archivoEscribir.txt', nuevoContenido, 'utf-8')
+console.log('Archivo escrito correctamente')
+
+
+//crear un nuevo fichero
+const contenidoNuevoArchivo = 'Contenido del nuevo archivo'
+await writeFile('./nuevoArchivo.txt', contenidoNuevoArchivo, 'utf-8')
+console.log('Nuevo archivo creado correctamente')
+
