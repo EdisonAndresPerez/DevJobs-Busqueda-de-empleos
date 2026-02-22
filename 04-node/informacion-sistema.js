b196a7840869ac1e61c19313471547d779ee6cc4
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-02-21 22:33:26 -0500

    subir cambios

--- 04-node/informacion-sistema.js
+++ 04-node/informacion-sistema.js
@@ -1,2 +1,13 @@
 
 
+import os from 'node:os';
+
+// Obtener información del sistema
+console.log('Información del sistema:');
+console.log(`Sistema operativo: ${os.type()} ${os.release()}`);
+console.log(`Arquitectura: ${os.arch()}`);
+console.log(`Número de CPUs: ${os.cpus().length}`);
+console.log(`Memoria total: ${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB`);
+console.log(`Memoria libre: ${(os.freemem() / (1024 ** 3)).toFixed(2)} GB`);
+    
+
