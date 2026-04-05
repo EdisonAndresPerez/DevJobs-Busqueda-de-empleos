105733b0ff0895912683a07823ebd7da82ee2009
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-04-05 14:17:28 -0500

    haciendo cambios live con --watch y enviando contenido HTML con la respuesta de la creacion del servidor

--- 04-node/server.js
+++ 04-node/server.js
@@ -1,16 +1,46 @@
-// importar modulo de http
+// Importamos el módulo HTTP nativo de Node.js
 import { createServer } from "node:http";
 
-//indiar el puerto
+// Definimos el puerto donde correrá el servidor
 const PORT = 3000;
 
-//crear el servidor
+/**
+ * Creamos el servidor HTTP
+ * 
+ * @param {IncomingMessage} peticion - Contiene la información de la solicitud del cliente (URL, método, headers, etc.)
+ * @param {ServerResponse} respuesta - Permite construir y enviar la respuesta al cliente
+ */
 const server = createServer((peticion, respuesta) => {
-  //configurar la respuesta
-  console.log("Petición recibida", peticion.url, peticion.method);
+
+    // Definimos el tipo de contenido que se enviará al navegador
+    respuesta.setHeader("Content-Type", "text/html");
+
+    // Enviamos contenido HTML en partes
+    respuesta.write("<h1>Hola Mundo</h1>");
+    respuesta.write("<p>Bienvenidos a mi servidor en Node.js</p>");
+    respuesta.write("<p>Todo esto sale del bootcamp de midudev. Muchas gracias</p>");
+
+
+    // Finalizamos la respuesta (obligatorio)
+    respuesta.end();
+
 });
 
-
+/**
+ * Iniciamos el servidor y lo ponemos a escuchar en el puerto definido
+ */
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+
+
+// --watch server.js
+
+
+//createServer() → crea el servidor
+//peticion → lo que llega del cliente (request)
+//respuesta → lo que tú envías (response)
+//setHeader() → defines cómo se interpreta la respuesta
+//write() → envías contenido por partes
+//end() → cierras la respuesta (sin esto, se queda colgado 😅)
+//listen() → levanta el servidor
