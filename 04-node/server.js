f439a234703871a34cc7d4f020b7d8f49e911de4
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-04-05 15:32:58 -0500

    crear usuarios con peticion POST de una API de node.js

--- 04-node/server.js
+++ 04-node/server.js
@@ -4,31 +4,48 @@ import { createServer } from "node:http";
 // Definimos el puerto donde correrá el servidor
 //const PORT = 3000;
 
-
 //la forma comun de usar es llamando la variable de entorno
-process.loadEnvFile() // Carga las variables de entorno desde el archivo .env
+process.loadEnvFile(); // Carga las variables de entorno desde el archivo .env
 const PORT = process.env.PORT || 3000;
 
 /**
  * Creamos el servidor HTTP
- * 
+ *
  * @param {IncomingMessage} peticion - Contiene la información de la solicitud del cliente (URL, método, headers, etc.)
  * @param {ServerResponse} respuesta - Permite construir y enviar la respuesta al cliente
  */
+
+
+//Lista de usuarios (simulada)
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
+
 const server = createServer((peticion, respuesta) => {
+  // Definimos el tipo de contenido que se enviará al navegador
+  respuesta.setHeader("Content-Type", "text/html");
 
-    // Definimos el tipo de contenido que se enviará al navegador
-    respuesta.setHeader("Content-Type", "text/html");
-
+  if (peticion.url === "/") {
     // Enviamos contenido HTML en partes
     respuesta.write("<h1>Hola Mundo</h1>");
     respuesta.write("<p>Bienvenidos a mi servidor en Node.js</p>");
-    respuesta.write("<p>Todo esto sale del bootcamp de midudev. Muchas gracias</p>");
-
+    respuesta.write(
+      "<p>Todo esto sale del bootcamp de midudev. Muchas gracias</p>",
+    );
+    return respuesta.end();
+  } else if (peticion.url === "/user") {
+    respuesta.setHeader("Content-Type", "text/html");
+    respuesta.write("<h1>Lista de Usuarios</h1>");
+    return respuesta.end(JSON.stringify(users));
+  } else {
+    respuesta.statusCode = 404;
 
     // Finalizamos la respuesta (obligatorio)
-    respuesta.end();
-
+    return respuesta.end("Pagina no encontrada");
+  }
 });
 
 /**
@@ -39,8 +56,19 @@ server.listen(PORT, () => {
 });
 
 
-// --watch server.js
 
+
+
+
+
+
+
+
+
+
+
+
+// --watch server.js
 
 //createServer() → crea el servidor
 //peticion → lo que llega del cliente (request)
