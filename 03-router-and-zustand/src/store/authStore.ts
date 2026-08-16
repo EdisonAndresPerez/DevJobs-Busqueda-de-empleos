c607a816c938244c3d50d1184a2ca4bf0c465afe
edison andres perez martinez <edisonandresperezmartinez@gmail.com>
2026-08-16 14:24:36 -0500

    actualizacion

--- 03-router-and-zustand/src/store/authStore.ts
+++ 03-router-and-zustand/src/store/authStore.ts
@@ -2,18 +2,41 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { useFavoriteStore } from "./favoriteStore";
 
-export const useAuthStore = create(
+interface User {
+  name: string;
+  lastName: string;
+  email: string;
+}
+
+interface AuthState {
+  isLoggedIn: boolean;
+  user: User | null;
+
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       //estado
       isLoggedIn: false,
+      user: null,
 
       //acciones
-      login: () => set({ isLoggedIn: true }),
+      login: (user) =>
+        set({
+          isLoggedIn: true,
+          user,
+        }),
+
       logout: () => {
         // Limpiamos los favoritos al cerrar sesión
         useFavoriteStore.getState().clearFavorites();
-        set({ isLoggedIn: false });
+        set({
+          isLoggedIn: false,
+          user: null,
+        });
       },
     }),
     {
